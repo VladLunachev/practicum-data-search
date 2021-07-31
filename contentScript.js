74c180fff8f1564ad5cce6a7c6fd2911016942ba
VladLunachev <v.lunachev@gmail.com>
2021-07-31 04:29:46 +0300

    -

--- contentScript.js
+++ contentScript.js
@@ -50,7 +50,7 @@ function start() {
           <p class="search-note-text">Understanding code logic, their concepts, and actions that you want to perform with them is more important than memorizing specific code syntax. Feel free to use the search bar and notes right away if you can't remember something, and don't feel bad about it. This knowledge will settle in your memory as you go through this course. <a class="search-note-link" href="https://rebrand.ly/memorization-strategy-explainer" target="_blank">You can watch this 5-minute video that explains it in more detail.</a></p>
           <br>
           <span class="search-note-title"><b>You can search here for any:</b></span>
-          <p class="search-note-text">● action you want to perform (like "add element" or "count unique values")<br> ● part of code (like "loc" or "format")<br> ● term that we use in our course materials (like "variable" or "library")<br></p>
+          <p class="search-note-text">● action you want to perform (like "add element" or "count unique values")<br> ● part of code (like "loc" or "format")<br> ● term or abbreviation that we use in our course materials (like "variable" or "df")<br></p>
           <br>
           <span class="search-note-title"><a class="search-note-link" href="https://rebrand.ly/clipboardfusion" target="_blank"><b>Also, you can try out the stand-alone app for creating a searchable personal knowledge base.</b></a></span>
           <br>
@@ -67,7 +67,7 @@ function start() {
     /*================== ENG to RU + change rebrandly links to RU sources =================*/
     var indexName = 'practicum_data_ru_search';
     var placeholder = 'Поиск';
-    var empty = `<div class="markdown markdown_type_theory markdown_size_small"><p class="search-empty">Sorry. No snippets on this topic yet. Send a messege to <a class="search-note-link" target="_blank" href='https://rebrand.ly/learning-support'> learning support</a></p></div>`;
+    var empty = `<div class="markdown markdown_type_theory markdown_size_small"><p class="search-empty">К сожалению, на этот запрос мы не нашли подсказок. <br>Вы можете <a class="search-note-link" target="_blank" href='https://rebrand.ly/learning-support-ru'> обратиться в службу учебной поддержки.</a></p></div>`;
     document.querySelector('#search').innerHTML = `
     <div id="search-container">
       <svg class="ais-SearchBox-submitIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
@@ -80,17 +80,17 @@ function start() {
     <div id="hits-container">
       <div id="search-note">
         <div id="search-note-card" class="markdown markdown_type_theory markdown_size_small">
-          <span class="search-note-title"><b>A note on memorization strategy:</b></span>
-          <p class="search-note-text">Understanding code logic, their concepts, and actions that you want to perform with them is more important than memorizing specific code syntax. Feel free to use the search bar and notes right away if you can't remember something, and don't feel bad about it. This knowledge will settle in your memory as you go through this course. <a class="search-note-link" href="https://www.youtube.com/watch?v=vP2MNhC_Igw&t=19s" target="_blank">You can watch this 5-minute video that explains it in more detail.</a></p>
+          <span class="search-note-title"><b>Небольшой совет по стратегии запоминания:</b></span>
+          <p class="search-note-text">Понимать логику кода, его концепции и действия, которые вы хотите выполнить — важнее, чем пытаться запоминать синтаксис кода. Если вспомнить какие-то детали не получается, без колебаний пользуйтесь поиском. Эти знания постепенно уложатся в вашей памяти по мере прохождения курса. <a class="search-note-link" href="https://rebrand.ly/memorization-advice-ru" target="_blank">Вот 5-минутное видео, раскрывающее этот совет подробнее.</a></p>
           <br>
-          <span class="search-note-title"><b>You can search here for any:</b></span>
-          <p class="search-note-text">● action you want to perform (like "add element" or "count unique values")<br> ● part of code (like "loc" or "format")<br> ● term or abbreviation that we use in our course materials (like "variable" or "df")<br></p>
+          <span class="search-note-title"><b>Здесь вы сможете искать любые:</b></span>
+          <p class="search-note-text">● действия, которые вы хотите выполнить (например, "добавить элемент" или "посчитать уникальные значения")<br>● фрагменты кода (например, "loc" или "format")<br>● термины и сокращения (например, "переменная" или "df")<br></p>
           <br>
-          <span class="search-note-title"><a class="search-note-link" href="https://www.notion.so/praktikum/ClipboardFusion-8fe44a7fc2774e30b6947d098dec5484" target="_blank"><b>Also, you can try out the stand-alone app for creating a searchable personal knowledge base.</b></a></span>
+          <span class="search-note-title"><a class="search-note-link" href="https://rebrand.ly/clipboardfusion-ru" target="_blank"><b>Также вы можете попробовать отдельное приложение для поиска и создания своей базы знаний.</b></a></span>
           <br>
           <br>
-          <span class="search-note-title"><b>Keyboard shortcuts:</b></span>
-          <p class="search-note-text">[ Ctrl+; ] to activate search <br> [ Ctrl+' ] to hide search <br> [ Esc ] to close search results <br> [ Shift+Enter ] to run code (search will automatically close) <br> [ Ctrl+Enter ] to submit answer (search will automatically close)</p>
+          <span class="search-note-title"><b>Клавиатурные сокращения:</b></span>
+          <p class="search-note-text">[ Ctrl+; ] активировать поиск <br> [ Ctrl+' ] спрятать поиск <br> [ Esc ] скрыть окно результатов поиска <br> [ Shift+Enter ] запустить выполнение кода (поиск автоматически закроется) <br> [ Ctrl+Enter ] отправить ответ на проверку (поиск автоматически закроется) </p>
         </div>
       </div>
       <div id="search-hits"></div>
@@ -135,7 +135,7 @@ function start() {
       templates: {
         item: `
           <div class='search-hits'>
-            <pre class="search-hit-text search-hit-description">{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</pre>
+            <pre class="search-hit-text search-hit-description hljs-comment">{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</pre>
             <pre class="search-hit-text search-hit-code">{{#helpers.highlight}}{ "attribute": "code_snippet" }{{/helpers.highlight}}</pre>
           </div>`,
         empty: empty,
