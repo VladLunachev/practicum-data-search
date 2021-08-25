c9cda54f8d2aa2a891637b3d9cb5cd6eee4838e0
VladLunachev <61316529+VladLunachev@users.noreply.github.com>
2021-08-25 16:53:08 +0300

    -

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-// russian lang test:
+// russian lang test
 // document.querySelector('html').lang = "ru"
 
 function modalPosition() {
@@ -35,7 +35,6 @@ function searchListeners() {
   document.querySelector('section.trainer-theory__tabs').style.zIndex = 300;
   document.querySelector('section.trainer-footer').style.marginBottom = '40px';
   document.querySelector('section.trainer-footer').style.zIndex = 250;
-  // document.querySelector('.notification').style.zIndex = 350; // makes notification above search (2 places)
   document.querySelector('#close-search').addEventListener('click', () => {
     closeSearch();
   });
@@ -81,13 +80,10 @@ function startSearch() {
     <p class="search-note-text">● action you want to perform (like "add element" or "count unique values")<br> ● part of code (like "loc" or "format")<br> ● term or abbreviation that we use in our course materials (like "variable" or "df")<br></p>
     <br>
     <span class="search-note-title"><b>A note on memorization strategy:</b></span>
-    <p class="search-note-text">Understanding code logic, their concepts, and actions that you want to perform with them is more important than memorizing specific code syntax. Feel free to use the search bar and notes right away if you can't remember something, and don't feel bad about it. This knowledge will settle in your memory as you go through this course. <a class="search-note-link" href="https://rebrand.ly/memorization-strategy-explainer" target="_blank">You can watch this 5-minute video that explains it in more detail.</a></p>
-    <br>
-    <span class="search-note-title"><a class="search-note-link" href="https://rebrand.ly/clipboardfusion" target="_blank"><b>Also, you can try out the stand-alone app for creating a searchable personal knowledge base.</b></a></span>
-    <br>
+    <p class="search-note-text">If you struggle to remember some syntax, it doesn't mean that you are failing. Feel free to use the search bar right away, and don't feel bad about it. This knowledge will settle in your memory as you go through this course. <a class="search-note-link" href="https://rebrand.ly/memorization-strategy-explainer" target="_blank">Learn more about memorization strategy.</a></p>
     <br>
     <span class="search-note-title"><b>Keyboard shortcuts:</b></span>
-    <p class="search-note-text">[ Ctrl+; ] to activate search <br> [ Ctrl+' ] to hide search <br> [ Esc ] to close search results <br> [ Shift+Enter ] to run code (search will automatically close) <br> [ Ctrl+Enter ] to submit answer (search will automatically close)</p>
+    <p class="search-note-text">[ Ctrl+; ] to activate search <br> [ Esc ] to close search results <br> [ Shift+Enter ] to run code (search will automatically close) <br> [ Ctrl+Enter ] to submit answer (search will automatically close)</p>
     </div>
     <div id="search-note-spacer"></div>
     </div>
@@ -115,13 +111,10 @@ function startSearch() {
     <p class="search-note-text">● действия, которые вы хотите выполнить (например, "добавить элемент" или "посчитать уникальные значения")<br>● фрагменты кода (например, "loc" или "format")<br>● термины и сокращения (например, "переменная" или "df")<br></p>
     <br>
     <span class="search-note-title"><b>Небольшой совет по стратегии запоминания:</b></span>
-    <p class="search-note-text">Понимать логику кода, его концепции и действия, которые вы хотите выполнить — важнее, чем пытаться запоминать синтаксис кода. Если вспомнить какие-то детали не получается, без колебаний пользуйтесь поиском. Эти знания постепенно уложатся в вашей памяти по мере прохождения курса. <a class="search-note-link" href="https://rebrand.ly/memorization-advice-ru" target="_blank">Вот 5-минутное видео, раскрывающее этот совет подробнее.</a></p>
-    <br>
-    <span class="search-note-title"><a class="search-note-link" href="https://rebrand.ly/clipboardfusion-ru" target="_blank"><b>Также вы можете попробовать отдельное приложение для поиска и создания своей базы знаний.</b></a></span>
-    <br>
+    <p class="search-note-text">Если вам трудно запомнить какой-нибудь синтаксис, это не значит, что вы не справляетесь. Это знание осядет в памяти, пока вы проходите курс. Просто пользуйтесь строкой поиска и не переживайте, со временем она перестанет быть вам необходима. <a class="search-note-link" href="https://rebrand.ly/memorization-advice-ru" target="_blank">Больше про стратегию запоминания.</a></p>
     <br>
     <span class="search-note-title"><b>Клавиатурные сокращения:</b></span>
-    <p class="search-note-text">[ Ctrl+; ] активировать поиск <br> [ Ctrl+' ] спрятать поиск <br> [ Esc ] скрыть окно результатов поиска <br> [ Shift+Enter ] запустить выполнение кода (поиск автоматически закроется) <br> [ Ctrl+Enter ] отправить ответ на проверку (поиск автоматически закроется) </p>
+    <p class="search-note-text">[ Ctrl+; ] активировать поиск <br> [ Esc ] скрыть окно результатов поиска <br> [ Shift+Enter ] запустить выполнение кода (поиск автоматически закроется) <br> [ Ctrl+Enter ] отправить ответ на проверку (поиск автоматически закроется) </p>
     </div>
     </div>
     <div id="search-hits"></div>
@@ -129,7 +122,15 @@ function startSearch() {
     `;
   }
 
-  const searchClient = algoliasearch('JAGBS2F5BN', '4bcd001dfae68e583fe1e651bad00297');
+  const searchClient = algoliasearch(
+    'JAGBS2F5BN',
+    '4bcd001dfae68e583fe1e651bad00297'
+    // {
+    //   headers: {
+    //     'X-Algolia-UserToken': '678910',
+    //   },
+    // }
+  );
   const search = instantsearch({
     indexName: indexName,
     searchClient,
@@ -172,7 +173,7 @@ function startSearch() {
   ]);
   search.addWidgets([
     instantsearch.widgets.configure({
-      hitsPerPage: 100, //* INFO
+      hitsPerPage: 500, //* INFO
     }),
   ]);
   search.start();
@@ -184,6 +185,17 @@ function closeSearch() {
   document.querySelector('.ais-SearchBox-input').blur();
   document.querySelector('#hits-container').style.display = 'none';
 }
+
+// function observer() {
+//   new MutationObserver(function(mutations) {
+//     console.log('mutation!');
+//     observer.disconnect();
+//     init()
+// }).observe(
+//     document.querySelector('.page__content'),
+//     { subtree: true, characterData: true, childList: true }
+// );
+// }
 
 function init() {
   var loading = setInterval(initSearch, 200);
@@ -208,6 +220,7 @@ function init() {
       modalPosition();
       document.querySelector('#hits-container').style.display = 'none';
       searchListeners();
+      // observer()
     }
   }
 }
